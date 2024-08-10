8c05fad3292754d38bb557c13cf92fc5cf6f8037
Kshitij Atram <atramkshitij6@gmail.com>
2024-08-10 18:37:51 +0530

    Data Analyst Portfolio
    I recently designed and developed a portfolio website for a data analyst friend. The site showcases their skills, projects, and professional journey with a clean, responsive design. It features interactive data visualizations, detailed case studies, and a skills section highlighting their expertise in tools like Python, SQL, and Tableau. I'm proud of how it turned out and excited to see how it will support their career.

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar() {
+    const mainHeader = document.querySelector(".mainHeader")
+    mainHeader.style.display = 'flex'
+    behavior: 'smooth'
+}
+
+function hideSidebar() {
+    const mainHeader = document.querySelector(".mainHeader")
+    mainHeader.style.display = 'none';
+    behavior: 'smooth'
+}
